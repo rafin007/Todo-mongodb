801bb19ae5fba54019a2ce8e0659d7989bb1ce97
Arefin <rafin.ryan.07@outlook.com>
2018-02-18 00:27:18 +0600

    Added POST /todos and route

--- server/server.js
+++ server/server.js
@@ -4,3 +4,23 @@ var {User} = require('./models/user');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
